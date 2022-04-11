f4b94d78f0f129a87d9162bf92e79e0b65ecbc0a
WarrenJW <wjwnetsolutions@gmail.com>
2022-04-11 14:54:38 +0100

    index.html

---
+++ script.js
@@ -0,0 +1,23 @@
+const choices= ['rock', 'paper', 'scissors']
+
+function game() {
+    // play the game
+    //play 5 rounds
+    //console based
+}
+
+function playRound() {
+
+    const playerSelection=
+}
+
+function playerChoice() {
+    // gets input from player
+}
+
+function computerChoice() {
+    //gets random input from cpu
+    return choices[Math.floor(Math.random()*choices.length)]
+}
+
+console.log(computerChoice())
